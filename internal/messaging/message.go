06c8c5110a0025e583f9aa73a8af06b62b5d3188
João Gabriel Soares <joao.soares@inchurch.com.br>
2025-02-12 18:39:46 -0300

    message send

--- internal/messaging/message.go
+++ internal/messaging/message.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/joaogabsoaresf/wpp-cli-bot/internal/api"
 	"github.com/joaogabsoaresf/wpp-cli-bot/internal/models"
+	"github.com/manifoldco/promptui"
 )
 
 type Message struct {
@@ -21,14 +22,45 @@ func MockMessages() []Message {
 }
 
 func ListMessageByChatId(chat models.Chat) {
+	// Limpa o terminal antes de exibir as mensagens
+	fmt.Print("\033[H\033[2J")
+
 	fmt.Println("\nMensagens Recentes:")
 	messages, err := api.ApiMessagesByID(chat.ID, 10)
 	if err != nil {
 		fmt.Printf("erro ao obter os recentes: %v", err)
 		return
 	}
+
 	fmt.Printf("Mensagens Recentes do(a) %s:\n", chat.Name)
 	for _, message := range messages {
 		fmt.Printf("\033[32m%-20s %-40s\033[0m\n", chat.Name, message.Content)
+	}
+
+	for {
+		prompt := promptui.Prompt{
+			Label: "Digite sua mensagem (ou :exit para sair)",
+			Validate: func(input string) error {
+				if input == ":exit" {
+					return nil
+				}
+				return nil
+			},
+		}
+
+		message, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("erro ao capturar a mensagem: %v\n", err)
+			return
+		}
+
+		if message == ":exit" {
+			fmt.Println("Saindo do modo de digitação...")
+			return
+		}
+
+		fmt.Print("\033[1A\033[K")
+
+		fmt.Printf("\033[34m%-20s %-40s\033[0m\n", chat.Name, message)
 	}
 }
